be6907e9db4de3a3f30dfa0664e28867d9920217
WeDontKnow3 <marquinhorelampago004@gmail.com>
2025-11-16 21:34:44 -0300

    Delete src/components/MonoEmoji.jsx

--- src/components/MonoEmoji.jsx
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-
-export default function MonoEmoji({ children, label = '', className = '', style = {} }) { return ( <span role="img" aria-label={label} className={mono-emoji ${className}} style={style} > {children} ); }
